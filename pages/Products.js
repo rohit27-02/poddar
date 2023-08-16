a74668fd02d099e3478d6cc6b3d87e3486e83407
rohit rawat <rohitrawatop@gmail.com>
2023-08-16 08:11:27 +0530

    amazon link

--- pages/Products.js
+++ pages/Products.js
@@ -2,15 +2,15 @@
 import React, { useState, useEffect } from 'react'
 
 const products = [
-    { img: "/1.jpg", img2: "/2.jpg", name: "Atom Harde" },
-    { img: "/4.jpg", img2: "/3.jpg", name: "Jeera Goli" },
-    { img: "/6.jpg", img2: "/5.jpg", name: "Pathar Hajam churan" },
-    { img: "/7.jpg", img2: "/8.jpg", name: "Anardana churan" },
-    { img: "/9.jpg", img2: "/10.jpg", name: "Hing Goli" },
-    { img: "/11.jpg", img2: "/12.jpg", name: "Arabpati Goli" },
-    { img: "/15.jpg", img2: "/16.jpg", name: "Kabali Hing Goli" },
-    { img: "/17.jpg", img2: "/18.jpg", name: "Podina Churan" },
-    { img: "/14.jpg", img2: "/13.jpg", name: "Rochak Munakka" },
+    { img: "/1.jpg", img2: "/2.jpg", name: "Atom Harde",amazon:"https://www.amazon.in/Poddar-Atom-Harade-100gm-Pack/dp/B0CF2GD9QN/ref=sr_1_49?keywords=Poddar+churans&qid=1692018412&sr=8-49" },
+    { img: "/4.jpg", img2: "/3.jpg", name: "Jeera Goli",amazon:"https://www.amazon.in/Poddar-Jeera-Goli-100-Pack/dp/B0CF2H2GVM/ref=sr_1_2_sspa?keywords=poddar+jeera+goli&qid=1692018766&sprefix=jeera+goli+poddar%2Caps%2C245&sr=8-2-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&psc=1" },
+    { img: "/6.jpg", img2: "/5.jpg", name: "Pathar Hajam churan",amazon:"https://www.amazon.in/Pddar-Pathar-Hajam-Churan-80gm/dp/B0BDVMJCSD/ref=sr_1_16?keywords=Poddar+churans&qid=1692018412&sr=8-16" },
+    { img: "/7.jpg", img2: "/8.jpg", name: "Anardana churan",amazon:"https://www.amazon.in/Poddar-Anardana-Churan-100gm-Pack/dp/B0BDVLQQPV/ref=sr_1_10?keywords=Poddar+churans&qid=1692018412&sr=8-10" },
+    { img: "/9.jpg", img2: "/10.jpg", name: "Hing Goli",amazon:"https://www.amazon.in/Poddar-Hing-goli-100-Pack/dp/B0965L5HFY/ref=sr_1_14?keywords=Poddar+churans&qid=1692018412&sr=8-14" },
+    { img: "/11.jpg", img2: "/12.jpg", name: "Arabpati Goli",amazon:"https://www.amazon.in/Poddar-Arabpati-Goli-Anardana-100gm/dp/B0BDVNCNZY/ref=sr_1_57?keywords=Poddar+churans&qid=1692018663&sr=8-57" },
+    { img: "/15.jpg", img2: "/16.jpg", name: "Kabali Hing Goli",amazon:"https://www.amazon.in/Poddar-Kabli-Hing-Goli-100gm/dp/B0CF2FY3CN/ref=sr_1_25?keywords=Poddar+churans&qid=1692018412&sr=8-25" },
+    { img: "/17.jpg", img2: "/18.jpg", name: "Podina Churan",amazon:"https://www.amazon.in/Poddar-podina-churan-gm-pack/dp/B08GBVKPL9/ref=sr_1_2?keywords=Poddar+churans&qid=1692018412&sr=8-2" },
+    { img: "/14.jpg", img2: "/13.jpg", name: "Rochak Munakka",amazon:"https://www.amazon.in/Poddar-Rochak-Munakka-100gm-Pack/dp/B0CF98HGHD/ref=sr_1_48?keywords=Poddar+churans&qid=1692018412&sr=8-48" },
 
 ]
 
@@ -55,7 +55,7 @@ const Products = () => {
                             </div>
                             <h1 className='font-semibold mb-4 text-gray-700'>{product.name}</h1>
                             <div className='flex max-sm:flex-col max-sm:space-y-4 items-center justify-center'>
-                                <a href=''><img className='w-16 mr-4 md:pr-2 border-gray-700 mt-2 md:border-r-2' src='/a1.webp' alt='amazon shop button'/></a>
+                                <a href={product.amazon}><img className='w-16 mr-4 md:pr-2 border-gray-700 mt-2 md:border-r-2' src='/a1.webp' alt='amazon shop button'/></a>
                                 <a href=''><img className='w-20 mr-4 md:pr-2 border-gray-700 md:border-r-2' src='/f1.png' alt='flipkart shop button'/></a>
                                 <a href='' className='h-7 rounded-md overflow-clip w-16'><img className='object-center -my-5 md:-my-4' src='/meesho.jpg' alt='flipkart shop button'/></a>
 
